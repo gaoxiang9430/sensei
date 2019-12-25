418e37f49431ec82528bb351903725096e73ee2e
GAO XIANG <gaoxiang9430@gmail.com>
2019-12-25 18:24:17 +0800

    update running script

--- augment/run.sh
+++ augment/run.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+set -x 
+
 dataset=gtsrb
 epoch=30
 model="1"
@@ -11,7 +13,7 @@ run_all()
 {
     for m in $model
     do
-        for approach in ga_loss # original replace30 replace_worst_of_10 ga_loss ga_cov
+        for approach in original replace30 replace_worst_of_10 ga_loss #ga_cov
         do
             log_file=execution_${dataset}_${m}_${approach}
             if $sa 
@@ -36,28 +38,34 @@ run_all()
 various_loss_threshold()
 {
     # threshold 1e-1, 1e-2 ... 1e-5
-    for i in 1 2 3 4 5
+    for m in $model
     do
-        m=3
-        flag=-o
-        rm -rf /tmp/config*
-        log_file=execution_${dataset}_${m}_ga_loss_t_${i}_${flag}.out
-        rm -rf $log_file
-        python augmented_training.py ga_loss $dataset -m $m -r $i -t 0 -e ${epoch} $flag 2>&1 | tee -a $log_file
-        python adversarial_attack.py ga_loss $dataset -m $m $flag 2>&1 | tee -a $log_file
+        for i in 1 2 3 4 5
+        do
+            m=3
+            flag=-o
+            rm -rf /tmp/config*
+            log_file=execution_${dataset}_${m}_ga_loss_t_${i}_${flag}.out
+            rm -rf $log_file
+            python augmented_training.py ga_loss $dataset -m $m -r $i -t 0 -e ${epoch} $flag 2>&1 | tee -a $log_file
+            python adversarial_attack.py ga_loss $dataset -m $m $flag 2>&1 | tee -a $log_file
+        done
     done
 }
 
 various_popsize()
 {
-    for queue in 3 5 10 15 20 30
+    for m in $model
     do
-        m=2
-        log_file=execution_${dataset}_${m}_ga_loss_q_${queue}.out
-        rm -rf /tmp/config*
-        rm -rf $log_file
-        python augmented_training.py ga_loss $dataset -m $m -q $queue -t 0 -e ${epoch} 2>&1 | tee -a $log_file
-        python adversarial_attack.py ga_loss $dataset -m $m 2>&1 | tee -a $log_file
+        for queue in 3 5 10 15 20 30
+        do
+            m=2
+            log_file=execution_${dataset}_${m}_ga_loss_q_${queue}.out
+            rm -rf /tmp/config*
+            rm -rf $log_file
+            python augmented_training.py ga_loss $dataset -m $m -q $queue -t 0 -e ${epoch} 2>&1 | tee -a $log_file
+            python adversarial_attack.py ga_loss $dataset -m $m 2>&1 | tee -a $log_file
+        done
     done
 }
 
@@ -90,18 +98,42 @@ config()
     fi
 }
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-    echo "USAGE: ./run.sh DATASET!"
+    echo -e "USAGE: ./run.sh DATASET [options]\n"
+    echo "  dataset: supported dataset: gtsrb, fashionmnist, cifar10, imdb, svhn"
+    echo "  options: supported options: sa (sensei_sa), st (six transformation), ps (various popsize), lt (different loss threshold)]!"
     exit 1
 fi
 
-#config gtsrb
-#config fashionmnist
-#config cifar10
-#config imdb
-#config svhn
+op=none
+if [ "$#" -ge 1 ]
+then
+    dataset=$1
+    shift
+fi
+if [ "$#" -ge 1 ]
+then
+    op=$1
+fi
 
-config $1
-run_all
+# config
+if [ $op = sa ]
+then
+    sa=true
+elif [ $op = st ]
+then
+    operator=6
+fi
+config $dataset
+
+if [[ "$op" == "ps" ]];
+then
+    various_popsize
+elif [[ "$op" == "lt" ]];
+then
+    various_loss_threshold
+else
+    run_all
+fi
 
